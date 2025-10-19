db50bf7cc865f3cc873afc9b54cfc8f40a2719c9
pinkiti <kaoru.nagata.3@gmail.com>
2025-10-20 03:43:45 +0900

    Fix add callback handler

---
+++ pages/api/oauth-callback.js
@@ -0,0 +1,9 @@
+export default async function handler(req, res) {
+  const { code } = req.query;
+
+  if (!code) {
+    return res.status(400).json({ error: 'Missing code parameter' });
+  }
+
+  return res.status(200).json({ message: 'Code received', code });
+}
